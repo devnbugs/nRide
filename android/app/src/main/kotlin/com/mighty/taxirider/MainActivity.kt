c0e65c52137c53d31c91cdb44bf9f65d79643b47
@devnbugs <devnbugs@gmail.com>
2025-04-22 17:35:23 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/mighty/taxirider/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.rabyte.nrideapp
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity: FlutterFragmentActivity() {
+}
